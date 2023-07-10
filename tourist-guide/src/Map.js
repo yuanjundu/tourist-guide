bb941c44d5ab73230ad6ed0410dd3faa3043972a
yuanjundu <yuanjundu9911@gmail.com>
2023-07-10 18:13:37 +0100

    Restrict map to Manhattan

--- tourist-guide/src/Map.js
+++ tourist-guide/src/Map.js
@@ -36,15 +36,6 @@ function Map() {
         console.log('Search box loaded:', searchBox);
     };
 
-    // Handle search place
-    // const handleSearchPlace = () => {
-    //     const places = searchBoxRef.current.getPlaces();
-    //     console.log(places)
-    //     if (places && places.length === 1) {
-    //         setPlaceDetails(places[0].name);
-    //     }
-    // }
-
     // Handle places changes
     const handlePlacesChanged = () => {
         // Remove the former markers
@@ -58,58 +49,44 @@ function Map() {
             return;
         }
 
-        // if (places && places.length === 1) {
-            // infoWindowRef.current = new window.google.maps.InfoWindow(); 
-            // // Set the content and open the info window
-            // infoWindowRef.current.setContent(places[0].name);
-            // infoWindowRef.current.setPosition(places[0].geometry.location);
-            // infoWindowRef.current.open(mapRef.current, places[0]);
-        // }
-        
-        let bounds = new window.google.maps.LatLngBounds();
-
-        places.forEach((place) => {
-            console.log(place.name)
-            // If doesn't find this place
-            if (!place.geometry || !place.geometry.location) {
-                console.log("Returned place contains no geometry");
-                return;
-            }
-
-            // Icons
-            const icon = {
-                url: place.icon,
-                size: new window.google.maps.Size(71, 71),
-                origin: new window.google.maps.Point(0, 0),
-                anchor: new window.google.maps.Point(17, 34),
-                scaledSize: new window.google.maps.Size(25, 25),
-            };
-
-            // Add new marker on map
-            markers.push(
-                new window.google.maps.Marker({
-                    map: mapRef.current,
-                    icon: icon,
-                    title: place.name,
-                    position: place.geometry.location
-                })
-            );
-
-            // Adjust the viewport of map
-            if (place.geometry.viewport) {
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
-
-            mapRef.current.fitBounds(bounds);
-
-        });
-
-        // // If only one place searched, then it can be added to itinerary
-        // if(places && places.length === 1){
-        //     setPlaceDetails(places[0].name);
-    // }
+        if(places){
+            places.forEach((place) => {
+                console.log(place.name)
+                // If doesn't find this place
+                if (!place.geometry || !place.geometry.location) {
+                    console.log("Returned place contains no geometry");
+                    return;
+                }
+    
+                // Icons
+                const icon = {
+                    url: place.icon,
+                    size: new window.google.maps.Size(71, 71),
+                    origin: new window.google.maps.Point(0, 0),
+                    anchor: new window.google.maps.Point(17, 34),
+                    scaledSize: new window.google.maps.Size(25, 25),
+                };
+    
+                // Add new marker on map
+                markers.push(
+                    new window.google.maps.Marker({
+                        map: mapRef.current,
+                        icon: icon,
+                        title: place.name,
+                        position: place.geometry.location
+                    })
+                );
+    
+                // // Adjust the viewport of map
+                if(places && places.length === 1){
+                    mapRef.current.setZoom(15);
+                    mapRef.current.panTo(place.geometry.location);
+                }else{
+                    mapRef.current.setZoom(11);
+                    mapRef.current.panTo(place.geometry.location);
+                }
+            });
+        }
     };
 
 
@@ -160,16 +137,15 @@ function Map() {
                 options={{
                     mapTypeControl: true,
                     mapTypeControlOptions: { style: window.google.maps.MapTypeControlStyle.DROPDOWN_MENU },
-                    // TO DO
-                    // restriction: {
-                    //     latLngBounds: {
-                    //         north: 40.8822,
-                    //         south: 40.6797,
-                    //         west: -74.0479,
-                    //         east: -73.9070
-                    //       },
-                    //     strictBounds: false
-                    //   }
+                    restriction: {
+                        latLngBounds: {
+                            north: 40.8822,
+                            south: 40.6797,
+                            west: -74.0479,
+                            east: -73.9070
+                          },
+                        strictBounds: false
+                      }
                 }}
             >
             <StandaloneSearchBox onLoad={handleSearchBoxLoad}   onPlacesChanged={handlePlacesChanged} >
