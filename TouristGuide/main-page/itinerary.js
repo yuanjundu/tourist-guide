4d96b2ebd57c6bfec967e25651f9554fab3793a7
Shubham Patil <53473177+shubh480@users.noreply.github.com>
2023-07-21 12:26:52 +0100

    Add files via upload

---
+++ TouristGuide/main-page/itinerary.js
@@ -0,0 +1,9 @@
+function toggle() {
+    var cont = document.getElementById('cont');
+    if (cont.style.display == 'block') {
+        cont.style.display = 'none';
+    }
+    else {
+        cont.style.display = 'block';
+    }
+}
