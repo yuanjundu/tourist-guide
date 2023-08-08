569a8f2efa83c31d2d7af882aa3c0bb387658afe
Shubham Patil <53473177+shubh480@users.noreply.github.com>
2023-08-09 00:54:51 +0100

    Update Itinerary.js

--- mysite/gmaps/react/src/pages/Itinerary.js
+++ mysite/gmaps/react/src/pages/Itinerary.js
@@ -3,18 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import axios from 'axios';
 import moment from 'moment';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import Header from '../components/Header';
 import Navigation from '../components/Navigation';
 import { refreshToken } from '../components/refreshToken';
 import styles from './Itinerary.module.css';
-import headStyles from './HeadDesign.module.css';
 import background from './assets/background.jpg'
-import morning from './assets/morning.jpg'
-import lunch from './assets/lunch.jpg'
-import evening from './assets/evening.jpg'
-import dinner from './assets/dinner.jpg'
-import { useSpring, animated } from '@react-spring/web'
+import {firstpara, restpara} from './ItineraryEnhance.js'
+import Card from './Card';
 
 const Itinerary = () => {
     const location = useLocation();
@@ -27,6 +23,11 @@ const Itinerary = () => {
     const [selectedLunchRestaurant, setSelectedLunchRestaurant] = useState(null);
     const [dinnerRestaurants, setDinnerRestaurants] = useState([]);
     const [selectedDinnerRestaurant, setSelectedDinnerRestaurant] = useState(null);
+    const local = "http://localhost:8000"
+    const midPoint = Math.floor(orderedAttractions.length / 2);
+    let para1=[firstpara(),restpara(),restpara(),restpara(),restpara()]
+    let para2 = [restpara(),restpara(),restpara(),restpara()]
+    const time = [["9:00-12:00"],["9:00-12:00","14:00-17:00"],["9:00-12:00","14:00-17:00","18:00-20:00"],["9:00-12:00","13:00-16:00","17:00-20:00","21:00-22:00"],["9:00-11:00","12:00-14:00","15:00-17:00","18:00-20:00","21:00-22:00"]];
 
     const fetchOptimalOrder = async () => {
         const token = localStorage.getItem('access');
@@ -109,8 +110,8 @@ const Itinerary = () => {
             const selectedLunchRestaurantData = lunchRestaurants.find(restaurant => restaurant.id === Number(selectedLunchRestaurant));
             const selectedDinnerRestaurantData = dinnerRestaurants.find(restaurant => restaurant.id === Number(selectedDinnerRestaurant));
     
-            console.log("selectedLunchRestaurantData" + selectedLunchRestaurantData);
-            console.log("selectedDinnerRestaurantData" + selectedDinnerRestaurantData);
+            // console.log("selectedLunchRestaurantData" + selectedLunchRestaurantData);
+            // console.log("selectedDinnerRestaurantData" + selectedDinnerRestaurantData);
     
             // add the new itinerary to the history
             const newItinerary = {
@@ -131,46 +132,48 @@ const Itinerary = () => {
             if (error.response && error.response.status === 401) {
                 refreshToken(handleSaveItinerary);
             } else {
-                console.log(error.response.data);
+                // console.log(error.response.data);
                 alert("An error occurred while saving your itinerary.");
             }
         });
     };
+    
+    
 
-    const springs = useSpring({
-        from: { x: 0 },
-        to: { x: 100 },
-      })
     
 
     return (
-        <div className={headStyles.container}>
-            <Header />
-            <div className={headStyles.image}>
-                <img className={headStyles.img} src={background}></img>
+        <div className={styles.container}>
+            {/* <Header /> */}
+            <div className={styles.image}>
+                <img className={styles.img} src={background}></img>
             </div>
-            <div className={headStyles.titleContainer}>
-                <h1 className={headStyles.title}>Itinerary</h1>
-                <p className={headStyles.intro}>for the day!</p>
+            <div className={styles.title}>
+                <p className={styles.itinerary}>In New York!</p>
+                <p className={styles.day}>concreate jungle where dreams are made of</p>
+                <p className={styles.day1}>There's nothing you can't do!</p>
             </div>
-            <div className={headStyles.container2}>
+            <div className={styles.container2}>
 
-                <div className={headStyles.container3}>
-                    
-                    <img className={styles.img2} src={morning}></img>                    
-                    <p className={styles.header}>10:00-12:00 Morning</p>
-                    <p className={styles.description}>Amidst Central Park's lush greenery, a leisurely stroll unveils urban serenity.</p>
-                    
-                    
+                <p className={styles.container2_title}>Your itinerary for the day</p>
+                <div className={styles.container3}>
+                                       
+                   
                     {morningAttractions.map((attraction, index) => (
-                        <div className={styles.attraction} key={index}>{attraction.name}</div>
+                        <div className={styles.container4}>  
+                            <img src= {local + attraction.image} alt={attraction.image} className={styles.attraction_img}/>
+                            <div className={styles.attraction} key={index}>{attraction.name}</div>
+                            <p className={styles.time}>{time[orderedAttractions.length-1][index]}</p>
+                            <p className={styles.container4_p} key={index}>{para1[index]}<b>{attraction.name}.</b></p>
+                            
+                        </div>
                     ))}
                 </div>
 
+               
 
-                <img className={styles.img3} src={lunch}></img>
-                <h2 className={styles.header}>14:00-16:00 Lunch</h2>
-                <p className={styles.description}>Indulge in classic lunch experience with mouthwatering deli sandwiches and a side of city charm.</p>
+
+                
                 <div className={styles.restcontainer}>
                 <select className={styles.restaurantSelector} value={selectedLunchRestaurant} onChange={(e) => handleSetLunchRestaurant(e.target.value)}>
                     <option value="">Select Restaurant</option>
@@ -182,16 +185,19 @@ const Itinerary = () => {
                 </select>
                 </div>
 
-                <img className={styles.img4} src={evening}></img>
+                {/* <img className={styles.img4} src={evening}></img>
                 <h2 className={styles.header}>18:00-20:00 Evening</h2>
-                <p className={styles.description}>A vibrant kaleidoscope of lights, entertainment, and bustling energy in the heart of Manhattan.</p>
+                <p className={styles.description}>A vibrant kaleidoscope of lights, entertainment, and bustling energy in the heart of Manhattan.</p> */}
                 {afternoonAttractions.map((attraction, index) => (
-                    <div className={styles.attraction} key={index}>{attraction.name}</div>
+                     <div className={styles.container4}>
+                     <img src= {local + attraction.image} alt={attraction.image} className={styles.attraction_img}/>
+                     <div className={styles.attraction} key={index}>{attraction.name}</div>
+                     <p className={styles.time}>{time[orderedAttractions.length-1][index+midPoint]}</p>
+                     <p className={styles.container4_p} key={index}>{para2[index]}<b>{attraction.name}.</b></p>
+                    </div>
                 ))}
 
-                <img className={styles.img3} src={dinner}></img>
-                <h2 className={styles.header}>20:00-22:00 Dinner</h2>
-                <p className={styles.description}>Treat yourself to a gourmet dinner in an upscale ambience while taking in the night views of the city.</p>
+                
                 <div className={styles.restcontainer}>
                     <select className={styles.restaurantSelector} value={selectedDinnerRestaurant} onChange={(e) => handleSetDinnerRestaurant(e.target.value)}>
                         <option value="">Select Restaurant</option>
@@ -202,14 +208,12 @@ const Itinerary = () => {
                         ))}
                     </select>
                 </div>
+                
                 <button className={styles.saveButton} onClick={handleSaveItinerary}>Save Itinerary</button>
 
             </div>
 
-        <div className='nav-box'>
             <Navigation />
-        </div>
-
         </div>
     );
 }
