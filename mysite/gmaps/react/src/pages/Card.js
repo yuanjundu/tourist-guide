2f5e88d29865d440ca86798a88190566d3e16fee
Shubham Patil <53473177+shubh480@users.noreply.github.com>
2023-08-10 16:00:36 +0100

    Update Card.js

--- mysite/gmaps/react/src/pages/Card.js
+++ mysite/gmaps/react/src/pages/Card.js
@@ -5,10 +5,12 @@ import { FaCheckCircle } from 'react-icons/fa';
 
 const local = `${process.env.REACT_APP_API_URL}/media/restaurants/`;
 
-const Card = ({ restaurant, title, index, onAddLunch, contact, isSelected, address, website }) => {
+const Card = ({ restaurant, title, index, onAddLunch, contact, address, website }) => {
+  const [isSelected, setSelected] = useState(false);
 
   const handleClick = () => {
-    if (!isSelected) {
+    setSelected(!isSelected); 
+    if (!isSelected){
       if (onAddLunch) {
         onAddLunch(restaurant);
       }
@@ -20,9 +22,10 @@ const Card = ({ restaurant, title, index, onAddLunch, contact, isSelected, addre
     <div className={`restaurant ${isSelected ? 'active' : ''}`} onClick={handleClick}>
       <img src= {local+index+".jpg"} className={styles.attraction_img}/>
       <h2 className={styles.cardname1}>{title}</h2>
+      {isSelected && <span className={styles.tick_icon}>✔</span>}
       <p className={styles.cardcontact}>Contact: {contact}</p>
       <p className={styles.cardaddress}>Address: {address}</p>
-      {isSelected && <MdDone className="tick_icon" />}
+      
       <a className={styles.cardwebsite} href={website}><p className={styles.moredetails}>More Details </p></a>
     </div>
   );
