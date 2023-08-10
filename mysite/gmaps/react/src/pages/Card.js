1117ff28aac469851ac96815347ea6df4ac81f90
Shubham Patil <53473177+shubh480@users.noreply.github.com>
2023-08-10 07:54:20 +0100

    Update Card.js

--- mysite/gmaps/react/src/pages/Card.js
+++ mysite/gmaps/react/src/pages/Card.js
@@ -20,8 +20,8 @@ const Card = ({ restaurant, title, index, onAddLunch, contact, isSelected, addre
     <div className={`restaurant ${isSelected ? 'active' : ''}`} onClick={handleClick}>
       <img src= {local+index+".jpg"} className={styles.attraction_img}/>
       <h2 className={styles.cardname1}>{title}</h2>
-      <p className={styles.cardname}>Contact: {contact}</p>
-      <p className={styles.cardname}>Address: {address}</p>
+      <p className={styles.cardcontact}>Contact: {contact}</p>
+      <p className={styles.cardaddress}>Address: {address}</p>
       {isSelected && <MdDone className="tick_icon" />}
       <a className={styles.cardwebsite} href={website}><p className={styles.moredetails}>More Details </p></a>
     </div>
